827e234e4d0f38126186fe5dfbf9f0a9e99fcb7f
Elia1996 <ribaldoneelia@gmail.com>
2024-07-15 21:34:52 +0200

    Started with augmented matrix

--- notai/intro.md
+++ notai/intro.md
@@ -1,1 +1,12 @@
 # AI Notes
+This notes are all about AI.
+
+I will call it AI even if it is not like that, it is not itelligence, it is just a simulation of it.
+
+No one knows something until he do it, so let's learn how to create fake intelligence and how to use it.
+
+I also like C/C++ and Verilog ;) but here we will use Python.
+
+All start from this
+
+![alt text](image.png)
--- notai/utils.py
+++ notai/utils.py
@@ -9,24 +9,30 @@ def float_to_fraction_latex(number,
     fraction = Fraction(number).limit_denominator()
     
     # Check if the conversion is close enough to the original number
-    if number == 0:
-        return True, "0"
-    elif abs(fraction - number) < tolerance:
+    if abs(fraction - number) < tolerance:
         return True, f"\\frac{{{fraction.numerator}}}{{{fraction.denominator}}}"
     else:
         return False, f"{number:.2f}"
 
 
-def mtolat(matrix: np.ndarray | np.matrix, bl_frac: bool = True) -> str:
+def mtolat(matrix: np.ndarray | np.matrix, bl_frac: bool = True, augmented: bool = False) -> str:
     latex_str = "\\begin{bmatrix}\n"
     for row in matrix:
         if isinstance(row, np.ndarray):
             row= np.array(row).flatten()
         blfracline = False
         blfrac = False
-        for elem in row:
+        for i, elem in enumerate(row):
+            if i == len(row) - 1 and augmented:
+                latex_str += " | "
             if isinstance(elem, int):
-                latex_str += f" {elem} &"
+                latex_str += f" {int(elem)} &"
+            if elem == 0:
+                latex_str += f" 0 &"
+            elif isinstance(elem, str):
+                latex_str += f"{elem} &"
+            elif elem - int(elem) == 0:
+                latex_str += f"{int(elem)} &"
             elif isinstance(elem, float):
                 if bl_frac:
                     blfrac, elem = float_to_fraction_latex(elem)
@@ -34,7 +40,7 @@ def mtolat(matrix: np.ndarray | np.matrix, bl_frac: bool = True) -> str:
                     elem = f"{elem:.2f}"
                 latex_str += f"{elem} &"
             else:
-                latex_str += f" {elem} &"
+                raise ValueError(f"Element {elem} is not an integer or float.")
             blfracline = blfracline or blfrac
         latex_str = latex_str[:-2] + "\\\\"
         if blfracline:
@@ -46,18 +52,61 @@ def mtolat(matrix: np.ndarray | np.matrix, bl_frac: bool = True) -> str:
 d_sub = {"@": r"\cdot", "frac": r"\frac",
          "[": r"\left[", "]": r"\right]",
          "(": r"\left(", ")": r"\right)",
-         " ": r"\,", "  ": r"\quad", "   ": r"\qquad"}
+         " ": r"\,", "  ": r"\quad", "   ": r"\qquad",
+         "->": r"\rightarrow", "<-": r"\leftarrow",
+         "-->": r"\longrightarrow", "<--": r"\longleftarrow"}
 
 def printalg(*args, debug=False, blfrac=True):
+    """Prints a list of strings and matrices in LaTeX format. The matrices are printed in a bmatrix environment.
+
+    Args:
+        *args: A list of strings or matrices, these are specific commands:
+            - "@" is replaced by "\cdot"
+            - "frac" is replaced by "\frac"
+            - "[" is replaced by "\left["
+            - "]" is replaced by "\right]"
+            - "(" is replaced by "\left("
+            - ")" is replaced by "\right)"
+            - " " is replaced by "\,"
+            - "  " is replaced by "\quad"
+            - "   " is replaced by "\qquad"
+            - "->" is replaced by "\rightarrow"
+            - "<-" is replaced by "\leftarrow"
+            - "-->" is replaced by "\longrightarrow"
+            - "<--" is replaced by "\longleftarrow"
+            - If the string "aug" precedes a matrix, the matrix is considered augmented
+                so if you run:
+                    printalg("aug", np.array([[1, 2, 5], [3, 4, 6]]))
+                you will get:
+                    \begin{bmatrix}
+                        1 & 2 &| 5 \\
+                        3 & 4 &| 6 \\
+                    \end{bmatrix}
+        debug: If True, the LaTeX code is printed.
+        blfrac: If True, the fractions are printed as fractions in the LaTeX code.
+    """
     txt = ""
+    bl_aug = False
     for arg in args:
         if isinstance(arg, str):
+            if arg == "aug":
+                bl_aug = True
+                continue
             ss = arg
             for k, v in d_sub.items():
                 ss = ss.replace(k, v)
             txt += ss
-        elif isinstance(arg, np.ndarray):
+        elif isinstance(arg, np.ndarray) or isinstance(arg, np.matrix) or isinstance(arg, list):
+            if bl_aug and isinstance(arg, np.matrix):
+                txt += mtolat(arg, bl_frac=blfrac, augmented=True)
+                bl_aug = False
+                continue
+            if isinstance(arg, list):
+                if not isinstance(arg[0], list):
+                    continue
             txt += mtolat(arg, bl_frac=blfrac)
+        else:
+            raise ValueError(f"Argument {arg} is not a string or a matrix.")
     if debug:
         print(txt)
     display(Math(txt))
